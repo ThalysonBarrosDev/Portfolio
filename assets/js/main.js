31ae8a86fdb6a7ecfaa77f98f43436cdc89d6c40
Thalyson Barros <thalysonbarrospereira2019@gmail.com>
2023-07-17 11:27:29 -0300

    STYLE: Implementando Header, Nav e Footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-/* JS */
+/* Responsiveness */
+var navUl = document.querySelector('.nav-ul');
+var menuIcon = document.querySelector('.nav-mobile-icon');
+
+menuIcon.addEventListener('click', () => {
+    if (navUl.classList.contains('active')) {
+
+        navUl.classList.remove('active');
+        document.querySelector('.nav-mobile-icon img').src = 'assets/img/menu-open.png';
+
+    } else {
+
+        navUl.classList.add('active');
+        document.querySelector('.nav-mobile-icon img').src = 'assets/img/menu-close.png';
+        
+    }
+});
